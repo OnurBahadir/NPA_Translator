725948d219a68207f4560799899a243298503c72
Onur Bahadır <onurbahadirr@gmail.com>
2020-07-29 03:29:38 +0300

    Update npa.h

--- npa.h
+++ npa.h
@@ -7,15 +7,16 @@
 #include <map>
 
 namespace npa {
+	
 
-   const std::map<std::string,std::string> alphabet{
-        {"a","Alpha"},{"b","Bravo"},{"c","Charlie"},{"d","Delta"},{"e","Echo"},{"f","Foxtrot"},
-        {"g","Golf"},{"h","Hotel"},{"i","India"},{"j","Juliet"},{"k","Kilo"},{"l","Lima"},
-        {"m","Mike"},{"n","November"},{"o","Oscar"},{"p","Papa"},{"q","Quebec"},{"r","Romeo"},
-        {"s","Sierra"},{"t","Tango"},{"u","Uniform"},{"v","Victor"},{"w","Whiskey"},{"x","X-ray"},
-        {"y","Yankee"},{"0","Zero"},{"1","One"},{"2","Two"},{"3","Three"},{"4","Four"},{"5","Five"},
-        {"6","Six"},{"7","Seven"},{"8","Eight"},{"9","Nine"},{".","Stop"},{"-","Dash"},{" ","(Space)"}
-    };
+   	const std::map<std::string,std::string> alphabet{
+        	{"a","Alpha"},{"b","Bravo"},{"c","Charlie"},{"d","Delta"},{"e","Echo"},{"f","Foxtrot"},
+        	{"g","Golf"},{"h","Hotel"},{"i","India"},{"j","Juliet"},{"k","Kilo"},{"l","Lima"},
+        	{"m","Mike"},{"n","November"},{"o","Oscar"},{"p","Papa"},{"q","Quebec"},{"r","Romeo"},
+        	{"s","Sierra"},{"t","Tango"},{"u","Uniform"},{"v","Victor"},{"w","Whiskey"},{"x","X-ray"},
+       	 	{"y","Yankee"},{"0","Zero"},{"1","One"},{"2","Two"},{"3","Three"},{"4","Four"},{"5","Five"},
+        	{"6","Six"},{"7","Seven"},{"8","Eight"},{"9","Nine"},{".","Stop"},{"-","Dash"},{" ","(Space)"}
+    	};
 
 
 	std::string Tolower( std::string s) {    
@@ -26,16 +27,16 @@ namespace npa {
 
 	std::string text2npa( const std::string& s ) {
 		std::string temp= Tolower(s);
-        std::string npa="";
+        	std::string npa="";
 		for ( auto &i : temp ) { 
-            std::stringstream ss;
-            ss<<i;
-            auto search = alphabet.find(ss.str());
-            if ( search != alphabet.end() ) {
-                npa+=search->second;
-                npa+=" ";
-            } 
-        }
+            		std::stringstream ss;
+            		ss<<i;
+            		auto search = alphabet.find(ss.str());
+            		if ( search != alphabet.end() ) {
+                		npa+=search->second;
+                		npa+=" ";
+            		} 
+        	}
 		return npa;
 	}
 
